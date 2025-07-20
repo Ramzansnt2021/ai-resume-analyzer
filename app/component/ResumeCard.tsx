aa8db776ea3cd20e6765c798a5aff0d7651d4496
Ramzansnt2021 <ramzansnt2019@gmail.com>
2025-07-20 20:44:54 +0500

    Code with Auth Functionality

---
+++ app/component/ResumeCard.tsx
@@ -0,0 +1,34 @@
+import { Link } from 'react-router'
+import ScoreCircle from '../component/ScoreCircle'
+const ResumeCard = ({
+  resume: { id, companyName, jobTitle, feedback, imagePath },
+}: {
+  resume: Resume
+}) => {
+  return (
+    <Link
+      to={`/resume/${id}`}
+      className='resume-card animate-in fade-in duration-1000 overflow-hidden'
+    >
+      <div className='resume-card-header'>
+        <div className='flex flex-col gap-2'>
+          <h2 className='!text-black font-bold break-words'>{companyName}</h2>
+          <h3 className='text-lg break-words text-gray-500'>{jobTitle}</h3>
+        </div>
+        <div className='flex-shrink-0'>
+          <ScoreCircle score={feedback.overallScore} />
+        </div>
+      </div>
+      <div className='gradient-border animate-in fade-in duration-1000'>
+        <div className='w-full h-full'>
+          <img
+            src={imagePath}
+            alt='resumes'
+            className='w-full h-[350px] max-h-[402px] object-cover rounded-2xl object-top'
+          />
+        </div>
+      </div>
+    </Link>
+  )
+}
+export default ResumeCard
